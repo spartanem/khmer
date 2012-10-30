3f3be9fc1a7be52ea1e72fa456fd41f2cd79d180
Eric McDonald <em@msu.edu>
2012-10-29 21:03:16 -0400

    Create hash table benchmarking code.

---
+++ lib/test-HashTables.cc
@@ -0,0 +1,93 @@
+// Simple C++ implementation of the 'load-graph' Python script.
+// Author: Eric A. McDonald
+
+
+#include <cstring>
+#include <cstdio>
+#include <cerrno>
+#include <cstdlib>
+#include <unistd.h>
+#include <error.h>
+#include <getopt.h>
+
+//#define OUTPUT_HASHTABLE
+
+#include "parsers.hh"
+#include "counting.hh"
+#include "primes.hh"
+
+using namespace std;
+using namespace khmer;
+
+
+static const char *	    SHORT_OPTS		= "k:N:x:";	
+
+
+int main( int argc, char * argv[ ] )
+{
+    unsigned long	kmer_length	    = 32;
+    float		ht_size_FP	    = 1.0E6;
+    unsigned long	ht_count	    = 4;
+    // unsigned long	input_chunk_size    = 104857600;
+
+    int			rc		    = 0;
+    int			opt		    = -1;
+    char *		conv_residue	    = NULL;
+    string		ofile_name;
+    string		ifile_name;
+    // FILE *		ofile		    = NULL;
+
+    while (-1 != (opt = getopt( argc, argv, SHORT_OPTS )))
+    {
+
+	switch (opt)
+	{
+	case 'k':
+	    kmer_length = strtoul( optarg, &conv_residue, 10 );
+	    if (!strcmp( optarg, conv_residue ))
+		error( EINVAL, EINVAL, "Invalid kmer length" );
+	    break;
+	case 'N':
+	    ht_count = strtoul( optarg, &conv_residue, 10 );
+	    if (!strcmp( optarg, conv_residue ))
+		error( EINVAL, EINVAL, "Invalid number of hashtables" );
+	    break;
+	case 'x':
+	    ht_size_FP = strtof( optarg, &conv_residue );
+	    if (!strcmp( optarg, conv_residue ))
+		error( EINVAL, EINVAL, "Invalid hashtable size" );
+	    break;
+	default:
+	    error( 0, 0, "Skipping unknown arg, '%c'", optopt );
+	}
+
+    }
+
+    if (optind < argc) ofile_name = string( argv[ optind++ ] );
+    else error( EINVAL, 0, "Output file name required" );
+
+    if (optind < argc) ifile_name = string( argv[ optind++ ] );
+    else error( EINVAL, 0, "Input file name required" );
+
+    HashIntoType	    ht_size		= (HashIntoType)ht_size_FP;
+    Primes primetab( ht_size );
+    vector<HashIntoType> ht_sizes;
+    for ( unsigned int i = 0; i < ht_count; ++i )
+	ht_sizes.push_back( primetab.get_next_prime( ) );
+
+    unsigned int	    reads_total		= 0;
+    unsigned long long int  n_consumed		= 0;
+    string		    ifile_name_STR( ifile_name );
+
+    CountingHash ht( kmer_length, ht_sizes );
+    ht.consume_fasta( ifile_name_STR, reads_total, n_consumed );
+
+#ifdef OUTPUT_HASHTABLE
+    ht.save( ofile_name + ".ht_count" );
+#endif
+
+    return rc;
+}
+
+
+// vim: set sts=4 sw=4 tw=80:
